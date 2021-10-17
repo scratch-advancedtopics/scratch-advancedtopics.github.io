b944f8b9718476d0ca6905efe559c6ec658379f8
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-10-17 09:10:24 +0800

    resolves #7

--- curators.py
+++ curators.py
@@ -1,11 +1,11 @@
 import urllib.request
 import json
 
-def get_users():
+def get_users(a):
 	offset = 0
 	out = []
 	while True:
-		contents = urllib.request.urlopen(f"https://api.scratch.mit.edu/studios/30152868/curators?offset={offset}").read()
+		contents = urllib.request.urlopen(f"https://api.scratch.mit.edu/studios/30152868/{a}?offset={offset}").read()
 		contents = contents.decode('utf-8')
 		if contents == '[]':
 			break
@@ -15,9 +15,12 @@ def get_users():
 
 def main():
 	out = []
-	for i in get_users():
+	for i in get_users('curators'):
+		i = json.loads(i)
+		out.append(i)
+	for i in get_users('manager'):
 		i = json.loads(i)
 		out.append(i)
 	return out
-with open('curators.json','w') as f:
+with open('members.json','w') as f:
 	json.dump(main(),f)
