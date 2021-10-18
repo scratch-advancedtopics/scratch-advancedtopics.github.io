05cbfac1bb9a5cfa1a294b89c5eab43295b5f4df
Micah Lindley <micahincorporated@gmail.com>
2021-10-18 23:15:00 +0000

    🔀 Attempt to manually merge changes

--- members.js
+++ members.js
@@ -1,17 +1,7 @@
-// fetch('https://api.github.com/orgs/scratch-advancedtopics/members')
-//   .then(res => res.json())
-//   .then(res => {
-    // const div = document.getElementById('members');
-    // for (let i = 0; i < res.length; i++) {
-    //   let el = document.createElement('div');
-    //   div.innerHTML += `<a href="${res[i].html_url}" class="exclude"><img src="${res[i].avatar_url}" class="avatar flex-item" title="${res[i].login}"></a>`
-    // }
-//   });
-
-fetch('curators.json')
+fetch('members.json')
   .then(res => res.json())
   .then(res => {
-    res = res[0];
+    res = res[0].concat(res[1]);
     const div = document.getElementById('members');
     for (let i = 0; i < res.length; i++) {
       let el = document.createElement('div');
